1f2cd5690c546a932a59e0c42b6e2816c92dadb3
Miquéias Garcia <mglucenalucena@gmail.com>
2021-05-17 20:48:58 -0300

    Projeto de recomendações simples

---
+++ 6º Exercise/5 - Acima da diagonal.py
@@ -0,0 +1,24 @@
+tamanhoMatriz = int(input("Informe a ordem da matriz: "))
+matriz = []
+
+for i in range(tamanhoMatriz):
+    matriz.append([0] * tamanhoMatriz)
+
+for i in range(tamanhoMatriz):
+    for j in range(tamanhoMatriz):
+        print("M[", i+1, ",", j+1, "]: ", sep="", end="")
+        matriz[i][j] = int(input())
+
+for i in range(tamanhoMatriz):
+    for j in range(tamanhoMatriz):
+        print(matriz[i][j], end=" ")
+    print()
+
+soma = 0
+
+for i in range(tamanhoMatriz):
+    for j in range(tamanhoMatriz):
+        if i < j:
+            soma += matriz[i][j]
+
+print("O valor da soma da diagonal principal é", soma)
